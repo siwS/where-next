c8f588b42b3c3009e534ab05f8240463a2cbcf55
Olya Gwenu <oli4ka.p@gmail.com>
2018-11-19 21:48:59 +0000

    Add start and debug sh files. Amend gitignore

---
+++ start.sh
@@ -0,0 +1,1 @@
+gradle build && java -jar build/libs/where-next.jar
---
+++ startDebug.sh
@@ -0,0 +1,1 @@
+gradle build && java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=8000,suspend=n -jar build/libs/where-next.jar
