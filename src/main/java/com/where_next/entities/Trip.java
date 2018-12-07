8856a2c85186d86c5eb4aeb293dc11a5a59db4be
sofia <tzi.sof@gmail.com>
2018-12-07 08:34:23 +0000

    Format spaces.

--- src/main/java/com/where_next/entities/Trip.java
+++ src/main/java/com/where_next/entities/Trip.java
@@ -1,18 +1,17 @@
 package com.where_next.entities;
 
-
 import javax.persistence.*;
 
 @Entity
-@Table(name="trip")
+@Table(name = "trip")
 public class Trip {
 
     // this initialValue is to avoid IntegrityExceptions when clashing with the ids from the import.sql.
     // we can remove later if not needed - thought it would be handy to put some seeding data there for now
     @Id
-    @SequenceGenerator(initialValue=3, name = "trip_id_seq", sequenceName = "trip_id_seq")
+    @SequenceGenerator(initialValue = 3, name = "trip_id_seq", sequenceName = "trip_id_seq")
     @GeneratedValue(strategy = GenerationType.AUTO, generator = "trip_id_seq")
-    @Column(name="id")
+    @Column(name = "id")
     private int id;
 
     @Column(name="name")
