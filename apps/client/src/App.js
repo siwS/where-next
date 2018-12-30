f25b1e0156c8f7edc7af80b5afd4da4ee9cc04f7
Olya Gwenu <oli4ka.p@gmail.com>
2018-12-30 23:33:35 +0000

    Small changes in the Client.

--- apps/client/src/App.js
+++ apps/client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './where_next_logo.png';
 import './App.css';
 
 class App extends Component {
@@ -9,16 +9,8 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            WhereNext <code>...coding in progress...</code> will be here :)
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
